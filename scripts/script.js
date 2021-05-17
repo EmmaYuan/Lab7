bc45b20db97728034b42f8a3da479fb7c52a8297
EmmaYuan <emmayyw@gmail.com>
2021-05-16 22:29:08 -0700

    finished lab 7

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,44 @@
-// script.js
+import { router } from './router.js';
 
-import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
-const setState = router.setState;
+window.addEventListener('popstate', (event) => {
+    router.setState(event.state, event.state.entry);
+});
 
-// Make sure you register your service worker here too
+const settingsIcon = document.querySelector('img[alt="settings"]');
+settingsIcon.onclick = () => {
+    router.setState('Settings');
+};
+
+const title = document.querySelector("header h1");
+title.onclick = () => {
+    router.setState('Home');
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        let newPost = document.createElement('journal-entry');
-        newPost.entry = entry;
-        document.querySelector('main').appendChild(newPost);
-      });
-    });
+    fetch('https://cse110lab6.herokuapp.com/entries')
+        .then(response => response.json())
+        .then(entries => {
+            entries.forEach(entry => {
+                const newPost = document.createElement('journal-entry');
+                newPost.entry = entry;
+                document.querySelector('main').appendChild(newPost);
+                const index = document.querySelector('main').childElementCount.toString();
+                entry.order = index;
+                newPost.onclick = () => {
+                    router.setState('Entry', entry);
+                };
+            });
+        });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
--- sw.js
+++ sw.js
@@ -1,6 +1,53 @@
-// sw.js - Service Worker
+let CACHE_NAME = 'my-site-cache-v1';
+let urlsToCache = [
+    'https://cse110lab6.herokuapp.com/entries'
+];
 
-// You will need 3 event listeners:
-//   - One for installation
-//   - One for activation ( check out MDN's clients.claim() for this step )
-//   - One for fetch requests
+self.addEventListener('install', (event) => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
